e5f9b0191034e6651c90727748548b0dbf0b2513
qiwen <huqiwen0313@gmail.com>
2019-04-09 17:50:32 -0400

    update codes to integrate gene expression

--- util/core.R
+++ util/core.R
@@ -8,6 +8,70 @@ library(glmulti)
 library(iRF)
 library(AUC)
 
+get_sample_info <- function(sample_file_name){
+  sample_info <- unlist(strsplit(sample_file_name, split = "[.]"))
+  tissue <- sample_info[1]
+  histone_marker <- sample_info[5]
+  time_point <- paste(sample_info[3], sample_info[4], sep = ".")
+  return(c(tissue, histone_marker, time_point))
+}
+
+get_total_reads <- function(sample_file_name){
+  input.dir <- file.path("data", "different_timepoints")
+  sample_reads <- read.table(file.path(input.dir, "allsample.reads.txt"),
+                             sep = "\t", header = FALSE)
+  sample <- gsub(".1.bam.sam.hm.signal", "", sample_file_name)
+  total_reads <- sample_reads[sample_reads[ ,1] == sample, 2]
+  return(total_reads)
+}
+
+cal_HM_signal_flanking <- function(HM_file, marker, total_reads, exp.flag=1, exp_file = NULL){
+  # caclulate chip-seq signals in the exon flanking regions
+  # Input:  exp.flag - 1, incoprate gene expression in the feature
+  
+  if(is.null(exp_file) & exp.flag == 1){
+    stop("please provide corespondent gene expression file")
+  }
+  
+  if(exp.flag == 1){
+    # normalization
+    gene.exp$cpm <- (gene.exp$count/sum(gene.exp$count)) * 1e6
+    gene.exp <- gene.exp[, c(1, 4)]
+    names(gene.exp) <- c("GeneID", "cpm")
+    HM_file <- merge(HM_file, gene.exp, by=c("GeneID"))
+  }
+  
+  HM_signal <- list()
+  for(i in 1:nrow(HM_file)){
+    if(HM_file[i,]$strand == "+"){
+      chip_left <- as.numeric(unlist(strsplit(as.character(HM_file[i, ]$chip_left), ",")))
+      chip_right <- as.numeric(unlist(strsplit(as.character(HM_file[i, ]$chip_right), ",")))
+    } else{
+      chip_left <- as.numeric(unlist(strsplit(as.character(HM_file[i, ]$chip_right), ",")))
+      chip_left <- rev(chip_left)
+      chip_right <- as.numeric(unlist(strsplit(as.character(HM_file[i, ]$chip_left), ",")))
+      chip_right <- rev(chip_right)
+    }
+    chip_left_intron <- (sum(chip_left[1:10]) / total_reads) * 1e6
+    chip_left_exon <- (sum(chip_left[11:20]) / total_reads) * 1e6
+    chip_right_intron <- (sum(chip_right[11:20]) / total_reads) * 1e6
+    chip_right_exon <- (sum(chip_left[1:10]) / total_reads) * 1e6
+    
+    if(exp.flag == 1){
+      HM_signal[[i]] <- data.frame(class = HM_file[i, ]$class, chip_left_intron = chip_left_intron,
+                                   chip_left_exon = chip_left_exon, chip_right_intron = chip_right_intron,
+                                   chip_right_exon = chip_right_exon, exp=HM_file[i, ]$cpm)
+    } else{
+      HM_signal[[i]] <- data.frame(class = HM_file[i, ]$class, chip_left_intron = chip_left_intron,
+                                   chip_left_exon = chip_left_exon, chip_right_intron = chip_right_intron,
+                                   chip_right_exon = chip_right_exon)
+    }
+  }
+  HM_signal <- dplyr::bind_rows(HM_signal)
+  names(HM_signal)[-1] = paste(marker, names(HM_signal)[-1], sep="_")
+  return(HM_signal)
+}
+
 setSeeds <- function(method = "cv", numbers = 1, repeats = 1, tunes = NULL, seed = 1237) {
   # This function is used to setup caret training random seeds for model reproducibility
   # B is the number of resamples and integer vector of M (numbers + tune length if any)
